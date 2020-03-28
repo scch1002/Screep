65b1fa4db47cf894a0007b7601a230b8e4280387
Chris Schwan <chrisschwan@outlook.com>
2020-03-28 15:37:51 -0500

    Fixed

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -3,15 +3,15 @@ export const harvester = {
 
         creep.say("harvest");
 
-        if(creep.carry.energy < creep.carryCapacity) {
+        if (creep.carry.energy < creep.carryCapacity) {
             const sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
         else {
-            if(creep.transfer(Game.spawns['CS1'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['CS1']);
+            if (creep.transfer(Game.spawns['CS'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['CS']);
             }
         }
     }
