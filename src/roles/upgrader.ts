d2117616f857aba266e640aadcff5dbf0d023f33
Chris Schwan <chrisschwan@outlook.com>
2020-03-28 22:12:04 -0500

    Corrected error

--- src/roles/upgrader.ts
+++ src/roles/upgrader.ts
@@ -17,6 +17,13 @@ export const upgrader = {
             if (room.controller && creep.upgradeController(room.controller) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
+        } else {
+            if (creep.carry.energy < creep.carryCapacity) {
+                const source = Game.getObjectById(creep.memory.assignedSource) as Source;
+                if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }
         }
     }
 };
