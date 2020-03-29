18d3517fb8191ac8f297768ba79b0fe74987d1c1
Chris Schwan <chrisschwan@outlook.com>
2020-03-28 22:07:27 -0500

    Corrected script

--- src/roles/builder.ts
+++ src/roles/builder.ts
@@ -4,25 +4,27 @@ export const builder = {
 
         creep.say("build");
 
-        if(creep.memory.building && creep.carry.energy === 0) {
+        const room = Game.spawns['CS'].room;
+
+        if (creep.memory.building && creep.carry.energy === 0) {
             creep.memory.building = false;
         }
-        if(!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
+        if (!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
             creep.memory.building = true;
         }
 
-        if(creep.memory.building) {
-            const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        if (creep.memory.building) {
+            const targets = room.find(FIND_CONSTRUCTION_SITES);
+            if (targets.length) {
+                if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
                 }
             }
         }
         else {
             const sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
             }
         }
     }
